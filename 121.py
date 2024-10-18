62827a8378e8a6e63700b23cf34e2caeb03f95ab
Eric Zhong <ejz25@cornell.edu>
2024-10-18 12:54:27 -0400

    finished 121

--- 121.py
+++ 121.py
@@ -1,3 +1,10 @@
 class Solution(object):
     def maxProfit(self, prices):
-        pass
+        max_price = 0
+        min_price = float("inf")
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_price:
+                max_price = price - min_price
+        return max_price
